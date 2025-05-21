8aec4e3426a2f5f52718b7a151afe87df8727217
jm.lee <jm.lee@mastor.co.kr>
2025-05-21 18:14:48 +0900

    Chapter 5

---
+++ Chapter5/IfIf/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IfIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자를 입력하세요 : ");
+            
+            String input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number > 0)
+            {
+                if (number == 0)
+                    Console.WriteLine("0보다 큰 짝수");
+                else
+                    Console.WriteLine("0보다 큰 홀수");
+            }
+            else
+            {
+                Console.WriteLine("0보다 작거나 같은 수");
+            }
+
+        }
+    }
+}
