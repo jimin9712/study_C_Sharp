c6b23e4edd93a21dff2d9cba8c93516ccb6e1a88
jm.lee <jm.lee@mastor.co.kr>
2025-05-23 15:56:36 +0900

    C# 교재 학습

---
+++ Chapter11/UsingDictionary/MainApp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingDictionary
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            dic["하나"] = "one";
+            dic["둘"] = "two";
+            dic["셋"] = "three";
+            dic["넷"] = "four";
+            dic["다섯"] = "five";
+
+            Console.WriteLine(dic["하나"]);
+            Console.WriteLine(dic["둘"]);
+            Console.WriteLine(dic["셋"]);
+            Console.WriteLine(dic["넷"]);
+            Console.WriteLine(dic["다섯"]);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        //                     
+        // 메시                                   호날두
+        //        사비                     데브라위너
+        //                    미켈렐레      
+        //카를로스        말디니        라모스        마르셀루
+        //                      부폰
+    }
+}
